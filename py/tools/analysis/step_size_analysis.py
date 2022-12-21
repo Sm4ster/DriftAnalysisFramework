bcf96e461362eeb857078710cc2fd31b3aa84f80
Stephan Frank <stephan.frank@ini.rub.de>
2022-12-21 19:19:25 +0100

    py: first results from symbolic regression

--- py/tools/analysis/step_size_analysis.py
+++ py/tools/analysis/step_size_analysis.py
@@ -1,22 +1,24 @@
 from DriftAnalysisFramework.OptimizationAlgorithms import CMA_ES
 from DriftAnalysisFramework.TargetFunctions import Sphere
 import numpy as np
+import feyn
+import pandas as pd
 import matplotlib.pyplot as plt
 from worker_module import analyze_step_size
 import time
 from tools.database.JobQueue import JobQueue
 
 # Globals
-sigma_iterations = 1000
-alg_iterations = 50000
-cutoff = 5000
+sigma_iterations = 500
+alg_iterations = 80000
+cutoff = 10000
 
 dimension = 2
 
 q = JobQueue("step_size_analysis")
 
 # Experiments
-results = np.empty([sigma_iterations, 4])
+results = np.empty([sigma_iterations, 2])
 
 target = Sphere(dimension)
 algorithm = CMA_ES(
@@ -30,7 +32,7 @@ algorithm = CMA_ES(
 )
 algorithm.set_location([1, 0])
 jobs = []
-for sigma_idx, sigma_22 in enumerate(np.linspace(0.0001, 10, num=sigma_iterations)):
+for sigma_idx, sigma_22 in enumerate(np.linspace(1, 10, num=sigma_iterations)):
     state = {
         "sigma": 3,
         "cov_m": np.array([[1, 1], [1, sigma_22]]),
@@ -46,7 +48,7 @@ while not q.finished():
 
 for job in q.jobs:
     result = job.result
-    results[job.meta["sigma_idx"]] = job.meta["sigma_22"], *result
+    results[job.meta["sigma_idx"]] = job.meta["sigma_22"], result[0]
 
 # Plotting, evaluating results
 plt.title("Middle value of converged Stepsize")
@@ -54,3 +56,10 @@ plt.xlabel("sigma_22")
 plt.ylabel("sigma*")
 plt.plot(results[:, 0], results[:, 1])
 plt.show()
+
+ql = feyn.QLattice()
+train = pd.DataFrame(results, columns=["sigma_22", "sigma*"])
+models = ql.auto_run(train, output_name='sigma*')
+
+best = models[0]
+best.plot(train, filename="feyn-summary.html")
