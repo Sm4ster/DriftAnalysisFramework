51a689c202f52ad5fd7894a60b03498ef07f83c6
Stephan Frank <stephan.frank@ini.rub.de>
2023-04-05 16:43:38 +0200

    py: transformation vectorized wip

--- py/tools/excentricity_analysis/excentricity_analysis_CMA.py
+++ py/tools/excentricity_analysis/excentricity_analysis_CMA.py
@@ -27,7 +27,6 @@ dim = 2
 
 
 def transform_to_normal_unvectorized(m, C, sigma):
-    print("\nInputs\n", m, C, sigma)
     # get the the transformation matrix
     A = np.linalg.eigh(C)[1]
 
@@ -36,17 +35,14 @@ def transform_to_normal_unvectorized(m, C, sigma):
     C_rot = A.T @ C @ A
     m_rot = A.T @ m
 
-    print("\nSingle A_T\n", A.T)
-
     # calculate the scaling factor which brings the covariance matrix to det = 1
     scaling_factor = 1 / (np.sqrt(np.linalg.det(C_rot)))
 
     m_normal = np.dot(m_rot, scaling_factor)
-    # print("\nSingle C_rot\n", C_rot)
-    # print("\nSingle scaling factor\n", scaling_factor)
-    # print("\nSingle m_normal\n", m_normal)
-
     C_normal = np.dot(C_rot, scaling_factor)
+
+    print("\nSingle C_normal\n", C_normal)
+    # print("\nSingle m_normal\n", m_normal)
 
     # The distance factor sets norm(m) = 1. To keep the proportion between the distance
     # of the center to the optimum and the spread of the distribution we adjust sigma.
@@ -76,25 +72,25 @@ def transform_to_normal_unvectorized(m, C, sigma):
 
 
 def transform_to_normal_vectorized(m, C, sigma):
-    print("\nInputs\n", m, C, sigma)
     # get the the transformation matrix
     A = np.linalg.eigh(C)[1]
 
     # rotate the coordinate system such that the eigenvalues of
     # the covariance matrix are parallel to the coordinate axis
     A_T = np.transpose(A, [0, 2, 1])
-    print("\n All A_T\n", A_T)
 
     C_rot = np.matmul(np.matmul(A_T, C), A)
     m_rot = np.einsum('...ij,...j->...i', A_T, m)
 
     # calculate the scaling factor which brings the covariance matrix to det = 1
     scaling_factor = 1 / (np.sqrt(np.linalg.det(C_rot)))
-    # print("\nAll C_rot\n", C_rot)
-    # print("\nAll scaling factors\n", scaling_factor)
 
-    m_normal = np.dot(m_rot, scaling_factor)
-    C_normal = np.dot(C_rot, scaling_factor)
+    m_normal = np.einsum('i,ij->ij', scaling_factor, m_rot)
+    C_normal = np.einsum('i,ijk->ijk', scaling_factor, C_rot) # np.dot(C_rot, scaling_factor, axis=1)
+
+    print("\nAll C_normal\n", C_normal)
+    # print("\nAll m_normal\n", m_normal)
+    # print("\nAll scaling_factors\n", scaling_factor)
 
     # The distance factor sets norm(m) = 1. To keep the proportion between the distance
     # of the center to the optimum and the spread of the distribution we adjust sigma.
@@ -128,9 +124,10 @@ def step_vectorized(m, C, sigma, z=None):
         # create standard normal samples and transform them
         z = np.random.standard_normal(m.shape)
 
-    # this is equivalent to Az in the normal form, as the matrix C is diagonal, therefore matrix A (with AA = C) is [[sqrt(C_00), 0][0, sqrt(C_11)]]
-    A_sigma = np.array([sigma * np.sqrt(C[:, 0, 0]), sigma * np.sqrt(C[:, 1, 1])]).T
-    x = m + z * A_sigma
+    # this is equivalent to Az in the normal form, as the matrix C is diagonal,
+    # therefore matrix A (with AA = C) is [[sqrt(C_00), 0][0, sqrt(C_11)]]
+    sigma_A = np.array([sigma * np.sqrt(C[:, 0, 0]), sigma * np.sqrt(C[:, 1, 1])]).T
+    x = m + z * sigma_A
 
     # evaluate samples
     fx = np.linalg.norm(x, axis=1, keepdims=True)
@@ -153,10 +150,13 @@ def step_unvectorized(m, C, sigma, z=None):
     if z is None:
         # step 1: draw random sample
         z = np.random.multivariate_normal(np.zeros(dim), cov_m)
-    x_t = m_t + sigma_t * z
+
+    # A = np.linalg.cholesky(C) in the general case
+    A = np.sqrt(C)
+    x_t = m_t + sigma_t * A @ z
 
     # step 2: update parameters
-    if np.linalg.norm(x_t) <= np.linalg.norm(m_t):
+    if np.linalg.norm(x_t) <= 1:  # np.linalg.norm(m_t) in the general case
         # a) Let x <- y
         m_t = x_t
         # b) update the success probability
@@ -175,28 +175,42 @@ def step_unvectorized(m, C, sigma, z=None):
     return m_t, cov_m, sigma_t
 
 
-def iterate_normal(alpha, sigma, kappa, num=10):
-    # expand inputs if necessary
-    if isinstance(alpha, (int, float)): alpha = np.tile(alpha, num)
-    if isinstance(sigma, (int, float)): sigma = np.tile(sigma, num)
-    if isinstance(kappa, (int, float)): kappa = np.tile(kappa, num)
+def iterate_normal(alpha, sigma, kappa, num=1000):
+    # Sanitize parameters #
+    # Determine which parameters are arrays and their lengths
+    is_array = [isinstance(param, np.ndarray) for param in [alpha, sigma, kappa]]
+    array_lengths = [len(param) if is_array[i] else None for i, param in enumerate([alpha, sigma, kappa])]
 
-    # check if all elements have equal length
-    shapes = [alpha.shape[0], sigma.shape[0], kappa.shape[0]]
-    if not all(element == shapes[0] for element in shapes):
-        raise Exception("")
+    if is_array.count(True) > 0:
+        # If there are multiple arrays, make sure they have the same length
+        unique_lengths = set(list(filter(lambda x: x is not None, array_lengths)))
+        if len(unique_lengths) > 1:
+            raise ValueError("All array parameters must have the same length.")
 
+        # Set length of parameters to expand to
+        array_index = is_array.index(True)
+        array_length = array_lengths[array_index]
+
+    else:
+        # Expand all numeric parameters to the same length given by num
+        array_length = num
+
+    # Expand all numeric parameters to its length
+    alpha, sigma, kappa = [np.tile(param, array_length)[:array_length] if not is_array[i] else param for i, param in
+                       enumerate([alpha, sigma, kappa])]
+
+    # Start the computations #
     # create m from alpha
     m = np.array([np.cos(alpha), np.sin(alpha)]).T
 
     # create C from kappa
-    C = np.zeros((num, 2, 2), dtype=float)
+    C = np.zeros((array_length, 2, 2), dtype=float)
     # C = np.full((num, 2, 2), 0.5, dtype=float)
     C[:, 0, 0] = kappa
     C[:, 1, 1] = (1 / kappa)
 
     # create the random samples
-    z = np.array([np.random.standard_normal(num), np.random.standard_normal(num)]).T
+    z = np.array([np.random.standard_normal(array_length), np.random.standard_normal(array_length)]).T
 
     # vectorized step
     start_time = time.perf_counter()
@@ -207,7 +221,7 @@ def iterate_normal(alpha, sigma, kappa, num=10):
 
     # unvectorized step
     start_time = time.perf_counter()
-    states_unvectorized = [np.empty([z.shape[0], 2]), np.empty([z.shape[0], 2, 2]), np.empty([z.shape[0]])]
+    states_unvectorized = (np.empty([z.shape[0], 2]), np.empty([z.shape[0], 2, 2]), np.empty([z.shape[0]]))
     for i in range(z.shape[0]):
         states_unvectorized[0][i], states_unvectorized[1][i], states_unvectorized[2][i] = step_unvectorized(m[i], C[i],
                                                                                                             sigma[i],
@@ -217,6 +231,12 @@ def iterate_normal(alpha, sigma, kappa, num=10):
     # print(f"The execution time of the unvectorized iteration is: {execution_time_unvec}")
 
     # print(f"Performance gain: {execution_time_unvec / execution_time_vec}")
+
+    print(states_vectorized, "\n", states_unvectorized, "\n",
+          np.array_equal(states_vectorized[0], states_unvectorized[0]),
+          np.array_equal(states_vectorized[1], states_unvectorized[1]),
+          np.array_equal(states_vectorized[2], states_unvectorized[2])
+          )
 
     # unvectorized transformation
     start_time = time.perf_counter()
@@ -241,4 +261,5 @@ def iterate_normal(alpha, sigma, kappa, num=10):
     # return np.arccos(new_m.T[0]), new_sigma, new_C[:,0,0]
 
 
-iterate_normal(np.linspace(1, 3, num=5), np.linspace(0.3, 0.5, num=5), np.linspace(5, 7, num=5), num=5)
+num = 5
+iterate_normal(np.linspace(1, 3, num=num), np.linspace(0.3, 0.5, num=num), np.linspace(1, 5, num=num))
