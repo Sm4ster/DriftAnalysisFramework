a1cd8816328d3c2ba49b9acdca7f18d88e877e81
Stephan Frank <stephan.frank@ini.rub.de>
2023-06-06 17:38:07 +0200

    py: expression parsing works exchange of variables wip

--- py/start_drift_analysis.py
+++ py/start_drift_analysis.py
@@ -1,13 +1,13 @@
 import numpy as np
 import numexpr as ne
 import re
-from collections import ChainMap
+
 from DriftAnalysisFramework.OptimizationAlgorithms import CMA_ES
 from DriftAnalysisFramework.TargetFunctions import Sphere
 from DriftAnalysisFramework.Transformations import CMA_ES as TR
 
 # potential function
-potential_function = "log(norm(m) + y, z) + exp(kappa+ ekl(f))"
+potential_function = "x + log(-norm(m) * - y, z * e) + exp(kappa+ ekl(f))"
 
 # config
 alpha_samples = 20
@@ -32,9 +32,12 @@ alg = CMA_ES(Sphere(), {
 })
 
 function_dict = {
-    "log": lambda x: np.log(x),
     "norm": lambda x: np.linalg.norm(x, axis=1)
 }
+
+exclude_list = ["where", "sin", "cos", "tan", "arcsin", "arccos", "arctan", "arctan2", "sinh", "cosh", "tanh",
+                "arcsinh", "arccosh", "arctanh", "log", "log10", "log1p", "exp", "expm1", "sqrt", "abs", "conj", "real",
+                "imag", "complex", "contains"]
 
 
 def parse_expression(expression):
@@ -45,6 +48,7 @@ def parse_expression(expression):
     ignore_operator = False
 
     for idx, char in enumerate(expression):
+
         if char == '(':
             if parenthesis == 0:
                 parsed_function.append(current_token)
@@ -52,6 +56,7 @@ def parse_expression(expression):
             else:
                 current_token += char
             parenthesis += 1
+
         elif char == ')':
             parenthesis -= 1
             if parenthesis == 0:
@@ -61,33 +66,34 @@ def parse_expression(expression):
                 current_token = ""
             else:
                 current_token += char
+
         elif char == ',':
-            if parenthesis == 0:
+            if parenthesis == 1:
                 parsed_function.append(parse_expression(current_token))
                 current_token = ""
             else:
                 current_token += char
+
         # identify operators
         elif parenthesis == 0 and char in "+-*/%&|~^<>!=":
             if current_token != "":
                 parsed_expression.append(current_token)
             current_token = ""
+
             if not ignore_operator:
                 current_token = char
 
                 # double char operators
-                if char in "<>=!*" and expression[idx+1] in "=<>*":
-                    current_token = expression[idx+1]
+                if char in "<>=!*" and expression[idx + 1] in "=<>*":
+                    current_token += expression[idx + 1]
                     ignore_operator = True
 
                 parsed_expression.append(current_token)
                 current_token = ""
-
-                # unary operator
-                # if char == "-":
             else:
                 ignore_operator = False
 
+        # add to the current token
         else:
             current_token += char
 
@@ -101,46 +107,56 @@ def parse_expression(expression):
 
 
 def replace_functions(potential_function, local_dict):
-    pattern = r'(\w+)\(([^()]*\))'
-    matches = re.findall(pattern, potential_function)
+    expression = ""
 
-    for match in matches:
-        function_name = match[0]
-        raw_arguments = match[1]
+    for token in potential_function:
+        # if the token is a tuple replace this function if necessary
+        if type(token) is tuple:
+            function_name = token[0]
 
-        # Count the number of opening and closing parentheses
-        open_parentheses = raw_arguments.count('(')
-        close_parentheses = raw_arguments.count(')')
+            if function_name in exclude_list:
+                argument_list = []
+                for tuple_token in token[1:]:
+                    if type(tuple_token) is list:
+                        argument_list.append(replace_functions(tuple_token, local_dict)[0])
+                    elif type(tuple_token) is tuple:
+                        argument_list.append(replace_functions([tuple_token], local_dict)[0])
+                    else:
+                        argument_list.append(tuple_token)
+                print(argument_list)
+                expression += function_name + "(" + ",".join(argument_list) + ")"
 
-        # Find the correct closing parenthesis
-        while close_parentheses < open_parentheses:
-            # Search for the next closing parenthesis
-            next_close_parenthesis = re.search(r'\)', raw_arguments)
-            if next_close_parenthesis:
-                raw_arguments = raw_arguments[next_close_parenthesis.start() + 1:]
-                close_parentheses += 1
+            elif function_name in function_dict:
+                argument_name_list = []
+                argument_list = []
+                for tuple_token in token[1:]:
+                    if type(tuple_token) is list:
+                        name = replace_functions(tuple_token, local_dict)[0]
+                        argument_name_list.append(name)
+                        argument_list.append(local_dict[name])
+                    else:
+                        argument_name_list.append(tuple_token)
+                        argument_list.append(local_dict[tuple_token])
+
+                variable_name = function_name + "_" + "_".join(argument_name_list)
+                local_dict[variable_name] = function_dict[function_name](*argument_list)
+                expression += variable_name
+
             else:
-                # If no closing parenthesis found, break the loop
-                break
+                raise Exception("Function is not defined: " + function_name, )
 
-        print(raw_arguments)
+        # if it is a list then parse the expression
+        elif type(token) is list:
+            expression += replace_functions(token, local_dict)[0]
+        else:
+            expression += token
 
-        # see if the argument has functions in it as well, replace if so
-        argument, _ = replace_functions(raw_arguments, local_dict)
-        potential_function = potential_function.replace(raw_arguments, argument)
-
-        # process this match by replacing
-        variable_name = function_name + "_" + argument
-        potential_function = potential_function.replace(function_name + "(" + argument + ")", variable_name)
-
-        # calculate the values and save them in the dict
-        local_dict[variable_name] = function_dict[function_name](local_dict[argument])
-
-    return potential_function, local_dict
+    return expression, local_dict
 
 
 def evaluate(expression, base_dict):
     parsed_expression = parse_expression(expression.replace(" ", ""))
+    replace_functions(parsed_expression, base_dict)
     test = 1
 
 
