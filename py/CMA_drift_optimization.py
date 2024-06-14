47080f5170ac4436692ca9ecd8046fc184fad818
Stephan Frank <stephan.frank@ini.rub.de>
2024-06-14 15:41:09 +0200

    modifications

--- py/CMA_drift_optimization.py
+++ py/CMA_drift_optimization.py
@@ -2,15 +2,20 @@ import numpy as np
 import json
 import cma
 
-drift_data_raw = json.load(open('./data/special_run.json'))
-drift_data = np.array(drift_data_raw["drift"]) + np.array(drift_data_raw["precision"])
-terms =  3 #drift_data.shape[3]
+drift_data_raw = json.load(open('./data/NEW_FULL_RUN_FAST_7_C_COV=0.02.json'))
+drift_data = np.array(drift_data_raw["drift"]) #+ np.array(drift_data_raw["precision"])
+terms = np.array([True, True, True, False, True, True, True, False])  # drift_data.shape[3]
+
+print(drift_data.shape)
 
 def c_drift(weights):
     cdrift = np.array(drift_data[:, :, :, 0])
 
-    for idx in range(1, terms):
-        cdrift += drift_data[:, :, :, idx] * weights[idx-1]
+    weight_idx = 0
+    for (idx, term) in enumerate(terms):
+        if term:
+            cdrift += drift_data[:, :, :, idx+1] * np.exp(weights[weight_idx])
+            weight_idx += 1
 
     return cdrift
 
@@ -19,8 +24,9 @@ def fitness(weights):
     cdrift = c_drift(weights)
     return cdrift.max()
 
+
 # Initial guess for the solution
-x0 = np.ones([terms-1]) * 0.5
+x0 = np.ones([terms.sum()]) * -0.5
 
 # Standard deviation for the initial search distribution
 sigma0 = 10  # Example standard deviation
@@ -39,7 +45,6 @@ best_solution = es.result.xbest
 
 # Fitness value of the best solution
 best_fitness = es.result.fbest
-
 
 print("Best weights vector: ", best_solution)
 print("Fitness value: ", best_fitness)
