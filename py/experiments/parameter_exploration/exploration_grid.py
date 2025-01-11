79f3776c740ae4aa4e0bbd5267c887bcd2ea72be
Stephan Frank <stephan.frank@rub.de>
2025-01-11 12:27:16 +0100

    fixed pathes

--- py/experiments/parameter_exploration/exploration_grid.py
+++ py/experiments/parameter_exploration/exploration_grid.py
@@ -1,8 +1,12 @@
 import numpy as np
+import os
+
+# Change the working directory
+os.chdir("/home/franksyj/DriftAnalysisFramework/py/")
 
 # List of parameter sets for each execution
-factor_1_sequence = np.geomspace(1 / 16, 2, num=6)
-factor_2_sequence = np.geomspace(1 / 16, 2, num=6)
+factor_1_sequence = np.geomspace(1 / 16, 4, num=7)
+factor_2_sequence = np.geomspace(1 / 16, 4, num=7)
 param_factors = np.array(np.meshgrid(factor_1_sequence, factor_2_sequence)).T.reshape(-1, 2)
 
-np.savetxt("../../configurations/parameter_exploration/factor_grid.txt", param_factors, delimiter=",")
+np.savetxt("configurations/parameter_exploration/factor_grid.txt", param_factors, delimiter=",")
