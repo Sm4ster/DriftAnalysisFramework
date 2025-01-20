98688a4dda91965935f072ec8f111011c0558ada
Stephan Frank <stephan.frank@rub.de>
2025-01-20 10:58:24 +0100

    added grid slicing

--- py/experiments/parameter_exploration/make_exploration_grid.py
+++ py/experiments/parameter_exploration/make_exploration_grid.py
@@ -5,8 +5,8 @@ import os
 os.chdir("/home/franksyj/DriftAnalysisFramework/py/")
 
 # List of parameter sets for each execution
-factor_1_sequence = np.geomspace(1 / 16, 4, num=7)
-factor_2_sequence = np.geomspace(1 / 16, 4, num=7)
+factor_1_sequence = np.geomspace(1 / 16, 2, num=6)
+factor_2_sequence = np.geomspace(1 / 16, 16, num=10)
 param_factors = np.array(np.meshgrid(factor_1_sequence, factor_2_sequence)).T.reshape(-1, 2)
 
 np.savetxt("configurations/parameter_exploration/exploration_grid.txt", param_factors, delimiter=",")
