447c948c6493b2016a5787f266f53a59dc5b58a1
Stephan <stephan.frank@rub.de>
2025-08-01 17:02:02 +0900

    removed the js part and cleaned up

--- js/src/db.js
+++
@@ -1,8 +0,0 @@
-// db.js
-import Dexie from 'dexie';
-
-export const db = new Dexie('DriftAnalysis');
-db.version(1).stores({
-  runs: "uuid", // Primary key and indexed props
-  locations: "uuid, location_id, run_id, vector, has_results",
-});
